6aecabef1cbefaace283bd35aba11074b34622b4
IsaacYav97 <riq.pacheco@yavirac.edu>
2026-05-29 14:00:30 -0500

    commit primero

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect
+import sqlite3
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    tasks = conn.execute('SELECT * FROM tasks').fetchall()
+    conn.close()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add():
+    task = request.form['task']
+
+    conn = get_db_connection()
+    conn.execute('INSERT INTO tasks (content) VALUES (?)', (task,))
+    conn.commit()
+    conn.close()
+
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
